3f17674e5023928e7c65ffb455aee94dc03a1e15
KarenQuezadaMejia <38744567+KarenQuezadaMejia@users.noreply.github.com>
2019-06-15 01:09:30 -0400

    archivos

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './App.css';
+import Frame from './Frame'
+
+
+class App extends Component {
+  render () {
+      return (
+        <div className="App">
+        <Frame/>
+      </div>  
+     );
+  }
+}
+export default App;
---
+++ src/component/BoxHeader.js
@@ -0,0 +1,62 @@
+import React, {Component} from 'react'
+import './BoxHeader.css';
+import BoxHeaderLogo from '../logo.png';
+import Button from './Button';
+import Waiter from './Waiter';
+import Kitchen from './Kitchen';
+
+class BoxHeader extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      showKitchen: false,
+      showWaiter: false,
+    }
+
+    this.WaiterFunc = this.WaiterFunc.bind(this);
+    this.KitchenFunc = this.KitchenFunc.bind(this);
+  }
+  WaiterFunc (){
+    this.setState({
+      ...this.state,
+      showWaiter : true,
+      showKitchen : false
+    })
+  }
+  KitchenFunc (){
+    this.setState({
+      ...this.state,
+      showWaiter : false,
+      showKitchen : true
+    })
+  }
+  
+  render () {
+    return (
+      <div>
+    <div className= "BoxHeader">
+      <img src={BoxHeaderLogo} className = "BoxHeaderLogo" alt="logo"/>
+      
+    </div>
+    <div id="options">
+    <Button rol = "Meser@" customOnButtonClick ={this.WaiterFunc}/>
+      <Button rol = "Cocina"customOnButtonClick ={this.KitchenFunc}/>
+    </div>
+    <div id="containerMenu">
+    
+      {
+        this.state.showWaiter && 
+        <Waiter></Waiter>
+      }
+      {
+        this.state.showKitchen &&
+        <Kitchen></Kitchen>
+      }
+    </div>
+    </div>
+    
+    );
+  }
+}
+
+export default (BoxHeader);
