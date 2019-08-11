2141104444b98195cc5b53e37b0d2453318c4ea6
KarenQuezadaMejia <quezadakaren16@gmail.con>
2019-08-11 12:53:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BurgerQueenRedux/precache-manifest.71ed292e1c8a124f2054c2d2862e0b46.js"
+  "/SCLO008-BurguerQueen/precache-manifest.bd9c2d67a564c6783a451c9d37c507f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BurgerQueenRedux/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SCLO008-BurguerQueen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
